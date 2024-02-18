ef70bc53b2a59827580d4a818ae15cfebfeb7eac
farouk <farouk.imami@gmail.com>
2024-02-18 19:37:02 +0100

    position

--- projet2_soltani_malki/projet2_nom1_nom2.py
+++ projet2_soltani_malki/projet2_nom1_nom2.py
@@ -1,3 +1,7 @@
+import pygame_min
+from typing import List
+from random import randint
+
 murs = [[1 , 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 1] , \
 [1 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 1] , \
 [1 , 0 , 1 , 1 , 0 , 1 , 1 , 1 , 0 , 1] , \
@@ -35,21 +39,22 @@ position_joueur = [5, 5]
 position_precedente_joueur = [0, 4]
 sortie = [0, 4]
 
-toto = "exemple2"
-
 def il_y_a_un_mur ( x : int , y : int ) -> bool :
     # on recupere la valeur a la position x,y du tableau et on verifie si il y a un 1.
     return murs[x][y] == 1
 
+
 def il_y_a_un_ennemi(x: int, y: int) -> bool :
     return ennemis[x][y] == 1
 
+
 def il_y_a_un_tresor(x: int, y: int ) -> bool :
-    return tresor[x][y] == 1
+    return tresors[x][y] == 1
 
 
-def obtenir_position_joueur() -> List[int]:
+def obtenir_position_joueur() -> List[int] :
     return position_joueur
+
 
 def deplacer_joueur ( x : int , y : int ) -> None :
     global position_joueur
@@ -58,38 +63,39 @@ def deplacer_joueur ( x : int , y : int ) -> None :
     position_joueur = [x, y]
     arriver_case()
 
-def arriver_case () -> None :
-    position_joueur[0]
-    position_joueur[1]
 
+def arriver_case() -> None :
+    global position_joueur
+    xPositionJoueur = position_joueur[0]
+    yPositionJoueur = position_joueur[1]
 
-    if (il_y_a_un_mur()) :
+    if (il_y_a_un_mur(xPositionJoueur, yPositionJoueur )) :
         print("il y a un mur")
 
-    if (il_y_a_un_ennemi()) :
+    if (il_y_a_un_ennemi(xPositionJoueur, yPositionJoueur)) :
         print("il y a un ennemi")
 
-    if (il_y_a_un_tresor()) :
+    if (il_y_a_un_tresor(xPositionJoueur, yPositionJoueur)) :
         print("il y a un tresor")
 
-    def jouer() -> None:
-        """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
+def jouer() -> None:
+    """
+    Description :
+    --------- ----
+    Cette fonction est la fonction principale de votre RPG .
+    Parametres :
+    ------------
+    Retours :
+    ---------
+    ( None )
+    """
 
     # On fait le lien avec les variables globales
     global position_joueur
     # On deplace le joueur sur sa position initiale
     x = position_joueur[0]
     y = position_joueur[1]
-    deplacer(x, y)
+    deplacer_joueur(x, y)
 
 
 jouer()
