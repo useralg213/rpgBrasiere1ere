3cf8b31bd19a6b30e2827dc2c2335bcf0b6ba882
farouk <farouk.imami@gmail.com>
2024-02-18 18:21:00 +0100

    premier commit

---
+++ projet2_soltani_malki/projet2_nom1_nom2.py
@@ -0,0 +1,98 @@
+murs = [[1 , 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 1] , \
+[1 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 1] , \
+[1 , 0 , 1 , 1 , 0 , 1 , 1 , 1 , 0 , 1] , \
+[1 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 0 , 1] , \
+[1 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 1] , \
+[1 , 0 , 1 , 0 , 1 , 0 , 0 , 1 , 0 , 1] , \
+[1 , 0 , 0 , 0 , 1 , 0 , 1 , 1 , 0 , 1] , \
+[1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 1 , 1] , \
+[1 , 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 1] , \
+[1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1]]
+
+ennemis = [[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0]]
+
+tresors = [[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0] , \
+[0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0]]
+
+position_joueur = [5, 5]
+position_precedente_joueur = [0, 4]
+sortie = [0, 4]
+
+
+def il_y_a_un_mur ( x : int , y : int ) -> bool :
+    #on recupere la valeur a la position x,y du tableau et on verifie si il y a un 1.
+    return murs[x][y] == 1
+
+def il_y_a_un_ennemi(x: int, y: int) -> bool :
+    return ennemis[x][y] == 1
+
+def il_y_a_un_tresor(x: int, y: int ) -> bool :
+    return tresor[x][y] == 1
+
+
+def obtenir_position_joueur() -> List[int]:
+    return position_joueur
+
+def deplacer_joueur ( x : int , y : int ) -> None :
+    global position_joueur
+    global position_precedente_joueur
+    position_precedente_joueur = position_joueur
+    position_joueur = [x, y]
+    arriver_case()
+
+def arriver_case () -> None :
+    position_joueur[0]
+    position_joueur[1]
+
+
+    if (il_y_a_un_mur()) :
+        print("il y a un mur")
+
+    if (il_y_a_un_ennemi()) :
+        print("il y a un ennemi")
+
+    if (il_y_a_un_tresor()) :
+        print("il y a un tresor")
+
+    def jouer() -> None:
+        """
+        Description :
+        --------- ----
+        Cette fonction est la fonction principale de votre RPG .
+        Parametres :
+        ------------
+        Retours :
+        ---------
+        ( None )
+        """
+
+    # On fait le lien avec les variables globales
+    global position_joueur
+    # On deplace le joueur sur sa position initiale
+    x = position_joueur[0]
+    y = position_joueur[1]
+    deplacer(x, y)
+
+
+jouer()
+
+
+
+
---
+++ projet2_soltani_malki/pygame_min.py
@@ -0,0 +1,141 @@
+"""
+@author: E. Brasier
+"""
+
+#### Importations
+import pygame
+from typing import List
+
+#### Variables globales
+# L'ecran du jeu
+screen = None
+# Les images a utiliser
+joueur_sprite = None
+tresor_sprite = None
+mur_sprite = None
+ennemi_sprite = None
+ennemi_tresor_sprite = None
+bordure_sprite = None
+# La taille de chaque case
+taille_case = 0
+
+#### Fonctions
+def initialiser_jeu (cote : int) -> None :
+    """
+    Description :
+    -------------
+    Cette fonction sert a demarrer la fenetre qui affichera votre RPG. Appelez cette fonction
+    dans le code de votre jeu, apres la creation de la carte du RPG.
+
+    Parametres :
+    ------------
+    - cote : Un entier qui exprime le nombre de cases de votre jeu en longueur et en largeur
+             (ce doit etre un carre).
+
+    Retours :
+    ---------
+    - (None) La fonction ne retourne rien. 
+    """
+    
+    # On fait le lien avec les variables globales
+    global screen
+    global joueur_sprite, tresor_sprite, mur_sprite, ennemi_sprite, bordure_sprite, \
+           ennemi_tresor_sprite
+    global taille_case
+    
+    # On initialise le jeu
+    pygame.init()
+    
+    # On definit la taille de la fenetre du jeu
+    ECRAN_COTE = 500
+    screen = pygame.display.set_mode([ECRAN_COTE, ECRAN_COTE])
+    
+    # On definit les sprites 
+    joueur_sprite = pygame.sprite.Sprite()
+    joueur_sprite.surf = pygame.image.load("images/joueur.png")
+    mur_sprite = pygame.sprite.Sprite()
+    mur_sprite.surf = pygame.image.load("images/mur.png")
+    tresor_sprite = pygame.sprite.Sprite()
+    tresor_sprite.surf = pygame.image.load("images/tresor.png")
+    ennemi_sprite = pygame.sprite.Sprite()
+    ennemi_sprite.surf = pygame.image.load("images/ennemi.png")
+    bordure_sprite = pygame.sprite.Sprite()
+    bordure_sprite.surf = pygame.image.load("images/bordure.png")
+    ennemi_tresor_sprite = pygame.sprite.Sprite()
+    ennemi_tresor_sprite.surf = pygame.image.load("images/ennemi_tresor.png")
+    
+    # On definit la taille des sprites
+    taille_case = ECRAN_COTE/cote
+    joueur_sprite.surf = pygame.transform.scale(joueur_sprite.surf, (taille_case, taille_case))
+    mur_sprite.surf = pygame.transform.scale(mur_sprite.surf, (taille_case, taille_case))
+    tresor_sprite.surf = pygame.transform.scale(tresor_sprite.surf, (taille_case, taille_case))
+    ennemi_sprite.surf = pygame.transform.scale(ennemi_sprite.surf, (taille_case, taille_case))
+    bordure_sprite.surf = pygame.transform.scale(bordure_sprite.surf, (taille_case, taille_case))
+    ennemi_tresor_sprite.surf = pygame.transform.scale(ennemi_tresor_sprite.surf, (taille_case, taille_case))
+    
+    
+def afficher_jeu(carte : List[List[int]], ennemis : List[List[int]], tresors : List[List[int]], position_joueur : List[int]) -> None :
+    """
+    Description :
+    -------------
+    Cette fonction sert a mettre a jour la fenetre qui affichera votre RPG. Appelez cette
+    fonction dans le code de votre jeu, a chaque fois que l'affichage doit changer, c'est-a-dire
+    a chaque fois que votre personnage se deplace.
+
+    Parametres :
+    ------------
+    - carte : Le tableau de tableaux d'entiers qui represente les zones libres et les zones murees.
+    - ennemis : Le tableau de tableaux d'entiers qui represente les zones avec des ennemis.
+    - tresors : Le tableau de tableaux d'entiers qui represente les zones avec des tresors.
+    - position_joueur : Le tableau d'entiers qui correspond aux coordonnees du joueur.
+
+    Retours :
+    ---------
+    - (None) La fonction ne retourne rien. 
+    """
+    
+    # On fait le lien avec les variables globales
+    global taille_case
+    
+    # On recupere les evenements de la fenetre pour qu'elle puisse se mettre a jour
+    pygame.event.get()
+    
+    # On choisit la couleur de fond de la fenetre
+    screen.fill((107, 150, 74))
+    
+    # On parcourt les coordonnees de la carte et on dessine les bons sprites aux bons endroits
+    for x in range(len(carte)) :
+        for y in range(len(carte[0])) :
+            if position_joueur[0] == x and position_joueur[1] == y :
+                screen.blit(joueur_sprite.surf, (y * taille_case, x * taille_case))
+            elif ennemis[x][y] >= 1 and tresors[x][y] >= 1 :
+                screen.blit(ennemi_tresor_sprite.surf, (y * taille_case, x * taille_case))
+            elif ennemis[x][y] >= 1 :
+                screen.blit(ennemi_sprite.surf, (y * taille_case, x * taille_case))
+            elif tresors[x][y] >= 1 :
+                screen.blit(tresor_sprite.surf, (y * taille_case, x * taille_case))
+            elif (x == 0 or y == 0 or x == len(carte) - 1 or y == len(carte[0]) - 1) and carte[x][y] != 0 :
+                screen.blit(bordure_sprite.surf, (y * taille_case, x * taille_case))
+            elif carte[x][y] == 1 :
+                screen.blit(mur_sprite.surf, (y * taille_case, x * taille_case))
+    
+    # On met a jour la fenetre
+    pygame.display.flip()
+    
+def quitter_jeu () -> None :
+    """
+    Description :
+    -------------
+    Cette fonction sert a fermer la fenetre qui affichera votre RPG. Appelez cette
+    fonction dans le code de votre jeu, au moment ou le jeu s'arrete.
+
+    Parametres :
+    ------------
+    
+    Retours :
+    ---------
+    - (None) La fonction ne retourne rien. 
+    """
+    
+    # On quitte le jeu
+    pygame.quit()
