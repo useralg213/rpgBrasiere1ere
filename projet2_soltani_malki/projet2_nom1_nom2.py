4b747d1f7bb13708fb8f0e7da532754c1c5d13e2
farouk <farouk.imami@gmail.com>
2024-02-27 11:37:30 +0100

    khbk

--- projet2_soltani_malki/projet2_nom1_nom2.py
+++ projet2_soltani_malki/projet2_nom1_nom2.py
@@ -60,73 +60,23 @@ dialogue_case_tresor = ["phrase case tresor 1",
                       "phrase case tresor 4",
                       "phrase case vide tresor 5"]
 def il_y_a_un_mur ( x : int , y : int ) -> bool :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     # on recupere la valeur a la position x,y du tableau et on verifie si il y a un 1.
     return murs[x][y] == 1
 
 
 def il_y_a_un_ennemi(x: int, y: int) -> bool :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     return ennemis[x][y] == 1
 
 
 def il_y_a_un_tresor(x: int, y: int ) -> bool :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     return tresors[x][y] == 1
 
 
 def obtenir_position_joueur() -> List[int] :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     return position_joueur
 
 
 def deplacer_joueur ( x : int , y : int ) -> None :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     global position_joueur
     global position_precedente_joueur
     position_precedente_joueur = position_joueur
@@ -135,16 +85,6 @@ def deplacer_joueur ( x : int , y : int ) -> None :
 
 
 def arriver_case() -> None :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     global position_joueur
     global murs
     global ennemis
@@ -205,16 +145,6 @@ def jouer() -> None:
         finDeplacement = deplacement()
     pygame_min.quitter_jeu()
 def sortir () -> None :
-    """
-        Description :
-        --------- ----
-        Cette fonction est la fonction principale de votre RPG .
-        Parametres :
-        ------------
-        Retours :
-        ---------
-        ( None )
-        """
     global ennemis_vaincus
     global tresors_collectes
     print("le nombre d'ennemi(s) vaincu(s) est de :", ennemis_vaincus)
@@ -337,20 +267,21 @@ def detruire_mur ( x : int , y : int ) -> None :
     murs[x][y] = 0
     print("vous avez detrui un mur !")
 
-def ramasser_tresor ( x : int , y : int ) -> None :"""
-    Description :
-    --------- ----
-    Cette fonction est la fonction principale de votre RPG .
-    Parametres :
-    ------------
-    Retours :
-    ---------
-    ( None )
+def ramasser_tresor ( x : int , y : int ) -> None :
     """
-global tresors_collectes
-tresors_collectes += 1
-print("vous venez d'acquerir un nouveau tresor : Bravo! ")
-print("le nombre de tresors collectés jusqu'à présent est de :", tresors_collectes)
+            Description :
+            --------- ----
+            Cette fonction est la fonction principale de votre RPG .
+            Parametres :
+            ------------
+            Retours :
+            ---------
+            ( None )
+            """
+    global tresors_collectes
+    tresors_collectes += 1
+    print("vous venez d'acquerir un nouveau tresor : Bravo! ")
+    print("le nombre de tresors collectés jusqu'à présent est de :", tresors_collectes)
 
 def affronter_ennemi ( x : int , y : int ) -> bool :
     """
@@ -415,11 +346,18 @@ def afficher_histoire() -> None  :
         """
     print("")
 
-
-def phraseTireeAuSort(categorie:int)-> str
+def phraseTireeAuSort(categorie: int) -> str :
     #si la categorie = 1 alors on a une phrase de type dialogue case vide .
-    if (categorie == 1) :
-        categorie
+    if (categorie == 1):
+    global dialogue_case_vide
+    #sert a prendre une phrase a une certaine position .
+    position = randint(0, 4)
+    dialogue_case_vide[position]
+
+
+
+
+
 
 
 
